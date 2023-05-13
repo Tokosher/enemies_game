a08f1bace21cb3f67305dca37129178b18c0f4f2
Illia <illia.tkach@isoftbet.com>
2023-05-13 16:25:25 +0300

    Simply added background and put animated enemy

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,64 @@
-import {
-  Application, Loader, Resource, Text, Texture, Ticker,
-} from 'pixi.js';
-import {
-  createEntity, getNextEntityDirection, getNextEntityPosition,
-} from './app/Entity';
+// TODO ADD A BORDER BLOCK AND COLLISION
+// FIX THE PROBLEM WITH RIGHT MOVING CHARACTER
+// todo add page on github
+import * as PIXI from 'pixi.js';
 
-// constants
-const SIZE = 512;
-const CENTER = SIZE / 2;
+const WIDTH = 600;
+const HEIGHT = 600;
 
-// create and append app
-const app = new Application({
-  width: SIZE,
-  height: SIZE,
-  backgroundColor: 0x1099bb, // light blue
-  sharedTicker: true,
-  sharedLoader: true,
+const app = new PIXI.Application({
+    width: WIDTH,
+    height: HEIGHT,
+    backgroundColor: 0x1099bb, // light blue
+    sharedTicker: true,
+    sharedLoader: true,
 });
 document.body.appendChild(app.view);
-const loader = Loader.shared;
-const ticker = Ticker.shared;
 
-// preload needed assets
-loader.add('samir', '/assets/img/hero.png');
+const data = require('./data/enemies.json'); // enemies data
+const loader = PIXI.Loader.shared;
 
-// when loader is ready
-loader.load(() => {
-  // create and append FPS text
-  const fps = new Text('FPS: 0', { fill: 0xffffff });
-  app.stage.addChild(fps);
+// adding a background // todo make a separate class for that
+// loader.add('background', './assets/background.png'); // todo add Names space with enemies / background
+// loader.load(onBackgroundLoaded);
 
-  // create and append hero
-  const heroTexture = loader.resources.samir.texture as Texture<Resource>;
-  const hero = createEntity(heroTexture, CENTER, CENTER);
-  app.stage.addChild(hero.sprite);
+function onBackgroundLoaded (loader, resources) {
 
-  // animate hero each "tick": go left or right continuously
-  ticker.add(() => {
-    fps.text = `FPS: ${ticker.FPS.toFixed(2)}`;
-    hero.direction = getNextEntityDirection(app.view.width, hero);
-    hero.sprite.x = getNextEntityPosition(hero);
-  });
-});
+}
+// loader.add('tileset', './assets/img/spritesheet.json')
+//     .load(onload)
+//
+// function  onload (loader, resources) {
+//     const dragonTexture = PIXI.Texture.from("monster.png");
+//     const sprite = new PIXI.Sprite(dragonTexture);
+//     sprite.position.set(200, 200);
+//     app.stage.addChild(sprite);
+// }
+loader.add('tileset', './assets/img/enemy/enemy-spritesheet.json')
+    .add('background', './assets/background.png')
+    .load(onload);
+
+function onload (loader, resources) {
+    const texture = resources.background.texture;
+    const background = new PIXI.Sprite(texture);
+    addChild(background);
+
+    const textures = [];
+    for (let i = 0; i < 3; i++) {
+        textures.push(PIXI.Texture.from(`left-step${i+1}.png`))
+    }
+
+    const enemy = new PIXI.AnimatedSprite(textures);
+    // enemy.position.set(200, 200);
+    enemy.scale.set(2, 2);
+    app.stage.addChild(enemy);
+
+    enemy.play();
+    enemy.animationSpeed = 0.1;
+}
+
+
+// utils
+function addChild (child) {
+    app.stage.addChild(child);
+}
