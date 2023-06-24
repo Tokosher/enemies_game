c29170c91c212e803c14eb995cd7413c11e09c2e
Illia <illia.tkach@igt.com>
2023-06-24 20:17:59 +0300

    Updates

--- components/base-visual-element.d.ts
+++
@@ -1,5 +0,0 @@
-import { Container } from 'pixi.js';
-import { CharacterData } from './character';
-export declare abstract class BaseVisualElement extends Container {
-    protected onPosition(position: CharacterData["position"]): void;
-}
